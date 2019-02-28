a35aaeadfd272aa8b743f52283dc621eb3978488
Renée Kooi <renee@kooi.me>
2019-02-28 11:32:57 +0100

    wololokingdoms: make build work

--- crates/wololokingdoms/build.rs
+++ crates/wololokingdoms/build.rs
@@ -6,6 +6,9 @@ fn main() {
         .build_target("all")
         .build();
 
+    println!("cargo:rustc-link-search=native={}/build/third_party/genieutils", dst.display());
     println!("cargo:rustc-link-search=native={}/build", dst.display());
     println!("cargo:rustc-link-lib=static=wololokingdoms");
+    println!("cargo:rustc-link-lib=static=genieutils");
+    println!("cargo:rustc-link-lib=dylib=stdc++");
 }
