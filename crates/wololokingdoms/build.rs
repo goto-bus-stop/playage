3b052c126d06cae1d2a3f9fc43a3bb057cc220cc
Renée Kooi <renee@kooi.me>
2020-05-26 19:59:43 +0200

    wololokingdoms: let it build on windows

--- crates/wololokingdoms/build.rs
+++ crates/wololokingdoms/build.rs
@@ -5,16 +5,17 @@ use std::{env, path::PathBuf};
 fn main() {
     println!("cargo:rerun-if-changed=../../third_party/wololokingdoms/libwololokingdoms");
 
-    let dst = Config::new("../../third_party/wololokingdoms/libwololokingdoms")
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let wk_path = Config::new("../../third_party/wololokingdoms/libwololokingdoms")
         .define("WK_STATIC_BUILD", "1")
-        .build_target("all")
+        .build_target("wololokingdoms")
         .build();
 
     println!(
         "cargo:rustc-link-search=native={}/build/third_party/genieutils",
-        dst.display()
+        wk_path.display()
     );
-    println!("cargo:rustc-link-search=native={}/build", dst.display());
+    println!("cargo:rustc-link-search=native={}/build", wk_path.display());
     println!("cargo:rustc-link-lib=static=wololokingdoms");
     println!("cargo:rustc-link-lib=static=genieutils");
     println!("cargo:rustc-link-lib=dylib=stdc++");
@@ -25,6 +26,6 @@ fn main() {
         .header("ffi.h")
         .generate()
         .expect("Unable to generate bindings")
-        .write_to_file(PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs"))
+        .write_to_file(PathBuf::from(out_dir).join("bindings.rs"))
         .expect("Unable to write bindings");
 }
